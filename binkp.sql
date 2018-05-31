584e5e7690e70808c13f0142d96d8b5f95aad8a5
Pavel Gulchuk <gul@gul.kiev.ua>
2018-05-31 10:20:24 +0300

    Set default for reset_freq and reset_last

--- binkp.sql
+++ binkp.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `node` char(16) NOT NULL,
   `passwd` char(16) NOT NULL,
-  `reset_freq` float NOT NULL,
-  `reset_last` int(10) unsigned NOT NULL,
+  `reset_freq` float unsigned NOT NULL DEFAULT 0,
+  `reset_last` int unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `node` (`node`)
 );
 CREATE TABLE IF NOT EXISTS `hosts` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int unsigned NOT NULL,
   `host` char(64) DEFAULT NULL,
-  `port` smallint(5) unsigned DEFAULT NULL,
+  `port` smallint unsigned DEFAULT NULL,
   UNIQUE (`id`,`host`,`port`),
   KEY (`id`),
   KEY (`host`)
